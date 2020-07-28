861f321169e478832365f38374c077cf1a4a1bb9
Dhanush <dhanu.r46@gmail.com>
2020-07-28 11:36:28 +0530

    Project Setup

---
+++ src/test/java/com/wizpanda/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizpanda.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
